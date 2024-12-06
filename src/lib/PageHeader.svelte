9913d17d03db2f995e5086d32831f64e289f1e6a
Juan Gonzalez <gonzalez.juanant524@csu.fullerton.edu>
2024-12-05 23:46:44 -0800

    fixed sign in buttons

--- src/lib/PageHeader.svelte
+++ src/lib/PageHeader.svelte
@@ -14,21 +14,13 @@
             {#if $auth === undefined}
                 <p>Loading...</p>
             {:else if $auth === null}
-                <button class="hover:underline" on:click={auth.sign_in}>Sign In</button>
 
                 <button class="hover:underline" on:click={redirectToLogin}>Sign In</button> <!-- Redirecting to login -->
             {:else}
-                <button class="hover:underline" on:click={auth.sign_out}>Sign Out</button> |
-                {$auth.email}
-                <img class="profile_pic" src={$auth.photoURL} alt="{$auth.displayName}" />
+                <button class="hover:underline" on:click={auth.sign_out}>Sign Out</button> 
             {/if}
         </nav>
     </header>
 </main>
 
-<style lang="postcss">
-    .profile_pic {
-        width: 50px;
-        height: 50px;
-    }
-</style>
+
